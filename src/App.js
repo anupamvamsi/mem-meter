03da8a3c57714b976cbdc58394f424dea1fb5c6f
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-22 16:42:06 +0530

    1. Currently looks good

--- src/App.js
+++ src/App.js
@@ -1,70 +1,35 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Pokemon } from './components/Pokemon';
 import { Random } from './lib/Random';
 
+let count = 0;
 function App() {
-  // async function fetchRandomPokemon() {
-  //   const pokeCards = document.querySelector('.poke-cards');
-  //   const pokeImg = document.createElement('img');
-  //   // console.log(app);
+  let [name, setName] = useState('uninit');
+  let [source, setSource] = useState('uninitURL');
 
-  //   try {
-  //     const poke = await fetch('https://pokeapi.co/api/v2/pokemon/905/');
-  //     const pokeJSON = await poke.json();
-  //     console.log(pokeJSON);
+  useEffect(() => {
+    async function fetchPoke() {
+      const pokeResponse = await fetch(
+        'https://pokeapi.co/api/v2/pokemon/905/'
+      );
+      const pokeResponseJSON = await pokeResponse.json();
 
-  //     pokeImg.src = pokeJSON.sprites.other['official-artwork'].front_default;
-  //     pokeImg.alt = pokeJSON.name;
+      console.log(count++);
+      setName(pokeResponseJSON.name);
+      setSource(
+        pokeResponseJSON.sprites.other['official-artwork'].front_default
+      );
 
-  //     console.log(pokeImg);
-  //   } catch {
-  //     pokeImg.alt = 'Unable to fetch Pokemon.';
-  //   }
-  //   pokeCards.appendChild(pokeImg);
-  // }
+      console.log(await name, await source);
+    }
 
-  // async function fetchRandomPokemon(id) {
-  //   const pokeCards = document.querySelector('.poke-cards');
-
-  //   const pokeImg = document.createElement('img');
-  //   const pokeCaption = document.createElement('p');
-  //   pokeImg.classList.add('pc-img');
-  //   pokeCaption.classList.add('pc-caption');
-
-  //   try {
-  //     const poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-  //     const pokeJSON = await poke.json();
-  //     console.log('pokeJSON:', pokeJSON);
-
-  //     pokeCaption.textContent = pokeJSON.name;
-  //     pokeImg.src = pokeJSON.sprites.other['official-artwork'].front_default;
-  //     pokeImg.alt = `An image of the Pokemon, ${pokeCaption.textContent}`;
-
-  //     console.log('pokeImg:', pokeImg);
-  //   } catch {
-  //     pokeCaption.textContent = 'Oops. No Pokemon here!';
-  //     pokeImg.alt = 'Unable to fetch Pokemon.';
-  //   }
-
-  //   console.log('pokeCardImg:', pokeImg);
-  //   console.log('pokeCardCaption:', pokeCaption);
-
-  //   pokeCards.appendChild(pokeImg);
-  //   return { pokeImg, pokeCaption };
-  // }
-
-  // useEffect(() => {
-  //   fetchRandomPokemon(Random.getRandomInt(1, 905));
-  // });
+    fetchPoke();
+  });
 
   return (
-    <div className="App">
-      <h1>Moshi Moshi</h1>
-      {/* <img src="" alt="Nothing loaded here yet." /> */}
-      <div className="poke-cards">
-        <Pokemon />
-      </div>
+    <div>
+      <img src={source} alt={name} />
     </div>
   );
 }
