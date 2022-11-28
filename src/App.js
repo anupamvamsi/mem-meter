72d35c8ca2851ba371b5f780a9e4919a05789402
Anupam Vamsi <anupam.ismav@gmail.com>
2022-11-28 22:06:22 +0530

    1. Add extra steps / functionality to algorithm, 2. Add explanation for randomization algorithm, 3. Renames

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ import { Loader } from './components/Loader';
 // const Pokedex = require('pokeapi-js-wrapper');
 // INSTEAD SAVE AND SERVE FIRST 30 POKEMON FROM YOUR OWN SITE PAGE? no point of API then, just take a random offset and fetch 30 pokemon?
 const NUM_POKE_MAX_FOR_GAME = 10;
-let NUM_POKE_IN_CURR_ROUND = 3;
+let NUM_POKE_IN_CURR_ROUND = 4;
+
+const ALLOWED_NUM_OF_SAME_POSITIONS = 0;
+const NUM_CHANGE_IN_POSITIONS =
+  NUM_POKE_IN_CURR_ROUND - ALLOWED_NUM_OF_SAME_POSITIONS;
 
 function App() {
   const [pokeArray, setPokeArray] = useState([]);
@@ -48,7 +52,7 @@ function App() {
         const initArray = [];
         for (let i = 0; i < NUM_POKE_IN_CURR_ROUND; i++) {
           const poke = await fetchPoke();
-          console.log(poke);
+          // console.log(poke);
           initArray.push(poke);
         }
 
@@ -73,26 +77,71 @@ function App() {
     setClickedPokeCards(clickedPokeCards.concat(clickedCard));
     console.log('setState (clickedArray) - triggering re-render');
 
-    // Copy over pokeArray into originalArray
-    const origPokeArray = pokeArray.map((poke) => poke);
+    /////////////////////////////////////////////////////////////////////
+    // RANDOMIZATION ////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
+    // Guaranteed to not let any one tile to be in the position it
+    // was in, before the click.
+    /////////////////////////////////////////////////////////////////////
+    // How does it work?
+    // 0. The current list of Pokemon (pokeArray) is copied into another,
+    //    `origPokeArray`, from which another temporary array `oldPos`
+    //    is created to remember the old (current) positions.
+    // 1. A for-loop runs for N number of times (N = number of Pokemon /
+    //    present on the UI).
+    // 2. Two random numbers are generated with ranges [0, N - 1 - M]
+    //    and [0, N], where M is the `i`th iteration of the loop.
+    // 3. The first random number (randomIdx1) is used to select and
+    //    extract a random Pokemon (extractedPoke) from the current list
+    //    of Pokemon.
+    // 4. This extracted random Pokemon is then added into a new list
+    //   (randomizedPokeArray) at a randomly selected index (randomIdx2).
+    // 5. With this new list, a check is performed using it along with
+    //    the temporary array.
+    // 6. If the randomized array has a Pokemon in the same position as
+    //    the old array, it is excluded from `checkArray`.
+    // 7. This means that, if the length of `checkArray` is equal to N,
+    //    then NONE of the Pokemon are occupying the same position before
+    //    the click (of a card) happened.
+    // 8. If you think that there can be X Pokemon with possibly the
+    //    same position as before, reduce N by X.
+    /////////////////////////////////////////////////////////////////////
     const randomizedPokeArray = [];
 
-    for (let i = 0; i < NUM_POKE_IN_CURR_ROUND; i++) {
-      const randomIdx = Random.getRandomInt(0, NUM_POKE_IN_CURR_ROUND - 1 - i);
+    // Copy over pokeArray into originalArray
+    const origPokeArray = pokeArray.map((poke) => poke);
+    // Array to check against, for successful randomization
+    const oldPos = origPokeArray.map((poke) => poke);
 
-      // Remove ONE (1) element from randomIdx
-      const pokeNewPosition = origPokeArray.splice(randomIdx, 1)[0];
-      randomizedPokeArray.push(pokeNewPosition);
+    for (let i = 0; i < NUM_POKE_IN_CURR_ROUND; i++) {
+      const randomIdx1 = Random.getRandomInt(0, NUM_POKE_IN_CURR_ROUND - 1 - i);
+      const randomIdx2 = Random.getRandomInt(0, NUM_POKE_IN_CURR_ROUND);
+
+      // Remove ONE (1) element from randomIdx1
+      const extractedPoke = origPokeArray.splice(randomIdx1, 1)[0];
+      // Remove ZERO (0) and add at randomIdx2
+      randomizedPokeArray.splice(randomIdx2, 0, extractedPoke);
     }
+
+    const checkArray = randomizedPokeArray.filter(
+      (newPos, i) => newPos.name !== oldPos[i].name
+    );
+
+    // console.log(
+    //   'equal?',
+    //   checkArray.length >= NUM_CHANGE_IN_POSITIONS,
+    //   checkArray
+    // );
+
+    if (checkArray.length < NUM_CHANGE_IN_POSITIONS) {
+      return randomizeCards(e);
+    }
+    // console.log('//////////////////// equal');
 
     setPokeArray(randomizedPokeArray);
     console.log(
       'setState (pokeArray to randomizedArray) - triggering re-render'
     );
-
-    // console.log('pokeArray:', pokeArray);
-    // console.log('originalArray:', originalArray);
-    // console.log('randomizedArray:', randomizedPokeArray);
   }
 
   // Called on every render
