e1e6bae04eabcdacb7d56c621580ba2f4995d957
Anupam Vamsi <anupam.ismav@gmail.com>
2022-12-19 12:10:04 +0530

    1. Update first and last PokeIDs as constants, 2. Update setCols to use double columns when two Poke exist

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,13 +1,22 @@
+const POKE_ID_FIRST = 1;
+const POKE_ID_LAST = 905;
 const NUM_POKE_MAX_FOR_GAME = 6;
 const NUM_BASE_POKE = 2;
 const ALLOWED_NUM_OF_SAME_POSITIONS = 0;
 
 const rootElement = document.querySelector(':root');
+
 function setCols(numPokeCurrRound) {
-  rootElement.style.setProperty('--cols', numPokeCurrRound / 2);
+  if (numPokeCurrRound === 2) {
+    rootElement.style.setProperty('--cols', numPokeCurrRound);
+  } else {
+    rootElement.style.setProperty('--cols', numPokeCurrRound / 2);
+  }
 }
 
 export {
+  POKE_ID_FIRST as PIDa,
+  POKE_ID_LAST as PIDz,
   NUM_POKE_MAX_FOR_GAME as POKEMAX,
   NUM_BASE_POKE as POKEBASE,
   ALLOWED_NUM_OF_SAME_POSITIONS as ALLOWEDPOS,
