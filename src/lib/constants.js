6d86d4d6708ab0bc62619ec6755b56ba52f42fc0
Anupam Vamsi <anupam.ismav@gmail.com>
2022-12-23 17:40:17 +0530

    1. Increase to 10 POKEMAX

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 const POKE_ID_FIRST = 1;
 const POKE_ID_LAST = 905;
-const NUM_POKE_MAX_FOR_GAME = 6;
+const NUM_POKE_MAX_FOR_GAME = 10;
 const NUM_BASE_POKE = 2;
 const ALLOWED_NUM_OF_SAME_POSITIONS = 0;
 
